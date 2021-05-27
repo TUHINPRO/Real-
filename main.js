ca6daf4521102f8f3576e4446ce39ecd0080a755
TUHINPRO <71925866+TUHINPRO@users.noreply.github.com>
2021-05-27 17:50:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+NoseX=0;
+NoseY=0;
+Difference=0;
+LeftWristX=0;
+RightWristX=0;
+
 function setup() {
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -15,8 +21,17 @@ function setup() {
     function gotPoses(results) {
     if(results.length>0) {
         console.log(results);
+        NoseX= results[0].pose.nose.x;
+    NoseY= results[0].pose.nose.y;
+    LeftWristX=results[0].pose.leftWrist.x;
+    RightWristX=results[0].pose.rightWrist.x;
+    Difference=LeftWristX-RightWristX;
     }
     }
     function draw() {
         background('#DC143C');
+        fill('#FFC0CB');
+        stroke(	'#FFC0CB');
+        textSize(Difference);
+        text("Tuhin",50,400);
     }
